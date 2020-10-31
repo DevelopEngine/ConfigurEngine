459d78cbacba24215a2830feebea34f39b545630
Alistair Chapman <alistair@agchapman.com>
2020-10-31 16:09:49 +1000

    Rebuild config extensions; Migrate build scripts to new structure; Add GitHub Actions, remove Travis CI;

--- src/ConfigurEngine/HostBuilderExtensions.cs
+++ src/ConfigurEngine/HostBuilderExtensions.cs
@@ -1,37 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using Microsoft.AspNetCore.Hosting;
+// using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 namespace ConfigurEngine
 {
     public static class HostBuilderExtensions
     {
-        internal static IWebHostBuilder ConfigureAppConfiguration(this IWebHostBuilder builder, string[] args, string variablePrefix = null) =>
-            builder.ConfigureAppConfiguration((ctx, config) =>
+        public static IConfigurationBuilder AddConfiguration(this IConfigurationBuilder config, string[] args, string variablePrefix = null) {
+            // config.AddConfigFile("config");
+            config.AddConfigFile("config", false);
+            if (args != null && args.Any())
             {
-                var env = ctx.HostingEnvironment;
+                config.AddCommandLine(args);
+            }
+            if (string.IsNullOrWhiteSpace(variablePrefix)) {
+                config.AddEnvironmentVariables();
+            } else {
+                config.AddEnvironmentVariables(variablePrefix);
+            }
+            return config;
+        }
 
-                config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                      .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
-                      .AddConfigFile("config");
 
-                if (args != null && args.Any())
-                {
-                    config.AddCommandLine(args);
-                }
-                if (variablePrefix == null) {
-                    config.AddEnvironmentVariables();
-                } else {
-                    config.AddEnvironmentVariables(variablePrefix);
-                }
-            });
-
-        private static IConfigurationBuilder AddConfigFile(this IConfigurationBuilder config, string fileName)
+        internal static IConfigurationBuilder AddConfigurationFile(this IConfigurationBuilder config, string fileName)
         {
             fileName = fileName.Replace(".json", string.Empty).Replace(".yml", string.Empty);
             config.AddJsonFile($"{fileName}.json", optional: true, reloadOnChange: true);
             config.AddYamlFile($"{fileName}.yml", optional: true, reloadOnChange: true);
             return config;
+        }
+
+        public static IConfigurationBuilder AddConfigFile(this IConfigurationBuilder configBuilder, string path, bool allVariants = true) {
+            var formats = new Dictionary<string, Action<IConfigurationBuilder, string>> {
+                [".json"] = (builder, p) => builder.AddJsonFile(p, true, true),
+                [".yml"] = (builder, p) => builder.AddYamlFile(p, true, true),
+                [".yaml"] = (builder, p) => builder.AddYamlFile(p, true, true)
+            };
+            path = Path.IsPathRooted(path) ? path : Path.GetFullPath(path);
+            if (Path.HasExtension(path)) {
+                var format = formats.TryGetValue(Path.GetExtension(path), out var confAction);
+                if (format) {
+                    confAction.Invoke(configBuilder, path);
+                }
+            } else {
+                path = path.TrimEnd('.');
+                if (allVariants) {
+                    foreach (var format in formats)
+                    {
+                        format.Value.Invoke(configBuilder, path + format.Key);
+                    }
+                } else {
+                    var matchedFiles = formats.Where(f => {
+                        return File.Exists(path + f.Key);
+                    });
+                    if (matchedFiles.Any()) {
+                        foreach (var match in matchedFiles)
+                        {
+                            match.Value.Invoke(configBuilder, path + match.Key);
+                        }
+                    }
+                }
+            }
+            return configBuilder;
         }
     }
 }
